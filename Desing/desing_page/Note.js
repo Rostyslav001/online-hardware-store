e1b1450dd46b3a7cdddfe2b385bf0e194226c4cd
Rostyslav001 <130447554+Rostyslav001@users.noreply.github.com>
2023-11-22 16:43:31 +0200

    Add files via upload

--- Desing/desing_page/Note.js
+++ Desing/desing_page/Note.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
+
     const gripViewButton = document.getElementById("grip-view");
     const listViewButton = document.getElementById("list-view");
     const productListView = document.getElementById("product_list");
-
+    const categorySelect = document.getElementById("categorySelect");
+    const productItems = document.querySelectorAll(".product");
+    
+    
     gripViewButton.addEventListener("click", function () {
         productListView.classList.remove("list-view");
         productListView.classList.add("grid-view");
@@ -15,18 +19,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
     productListView.classList.remove("list-view");
     productListView.classList.add("grid-view");
-});
 
-const categorySelect = document.getElementById("categorySelect");
-const productItems = document.querySelectorAll(".product");
-categorySelect.addEventListener("change", function () {
-    const selectedCategory = categorySelect.value;
-    productItems.forEach((product) => {
-        if (selectedCategory === "all" || product.dataset.category === selectedCategory) {
-            product.style.display = "block";
-        } else {
-            product.style.display = "none";
-        }
-    });
-});
-
+}); 
--- Desing/desing_page/filter.js
+++ Desing/desing_page/filter.js
@@ -1,39 +1,118 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const colorPicker = document.getElementById("colorPicker");
     const addFilter = document.getElementById("applyFilter");
-    const cleatFilter = document.getElementById("clear-filter");
-    const selectedColorsContainer = document.querySelector(".selectedColorsContainer");
+    const categorySelect = document.getElementById("categorySelect");
+    const clearFilter = document.getElementById("clear-button");
+    const priceList = document.getElementById("price-list");
+    const productItems = document.querySelectorAll(".product");
+    const allFilter = document.getElementById("allcategory");
+    const filterwindows = document.getElementById("filterWindow");
+    const filterButton = document.getElementById("filterButton");
+    const filterNameInput = document.getElementById("filterName");
+    const filtersContainer = document.querySelector(".filter-selected");
+    const selectedPriceRange = priceList.value;
 
     addFilter.addEventListener("click", () => {
-        const selectedColors = Array.from(colorPicker.selectedOptions).map(option => option.value);
+        const selectedCategories = Array.from(categorySelect.selectedOptions).map(option => option.value);
+        const selectedCategoriesSet = new Set(selectedCategories);
 
-        selectedColors.forEach(color => {
-            if (color !== "all") {
-                const colorElement = document.createElement("div");
-                colorElement.classList.add("selected-color");
-                colorElement.textContent = color;
-                selectedColorsContainer.appendChild(colorElement);
+        if (selectedCategoriesSet.has("all")) {
+            productItems.forEach(product => {
+                product.style.display = "block";
+            });
+        } else {
+            productItems.forEach(product => {
+                const productCategory = product.dataset.category;
+                product.style.display = selectedCategoriesSet.has(productCategory) ? "block" : "none";
+            });
+        }
 
-                const deleteButton = document.createElement("remove-option");
-                deleteButton.textContent = "Delete";
-                deleteButton.addEventListener("click", () => {
-                   
-                    selectedColorsContainer.removeChild(colorElement);
+        selectedCategories.forEach(category => {
+            if (category !== "all" && !filtersContainer.querySelector(`.selected-category[title='${category}']`)) {
+                const categoryElement = document.createElement("div");
+                categoryElement.classList.add("selected-category");
+                categoryElement.textContent = category;
+                categoryElement.title = category;
+                filtersContainer.appendChild(categoryElement);
+
+                const icon = document.createElement("img");
+                icon.src = "D:/site_shop/Desing/imag/icon/Group 120.svg";
+                icon.alt = "Delete";
+                icon.style.width = "24px";
+                icon.style.height = "24px";
+                icon.style.marginLeft = "5px";
+                categoryElement.appendChild(icon);
+
+                icon.addEventListener("click", () => {
+                    filtersContainer.removeChild(categoryElement);
                 });
-
-                colorElement.appendChild(deleteButton);
-                selectedColorsContainer.appendChild(colorElement);
-                deleteButton.style.marginLeft = "10px";
-                deleteButton.style.backgroundColor = "red"
-                
-            
             }
         });
 
-        
+        selectedPriceRange.forEach(price => {
+            if (price !== "custom-option") {
+                // Check if the category filter already exists
+                if (!filtersContainer.querySelector(".selected-price[title='" + price + "']")) {
+                    const priceList = document.createElement("div");
+                    priceList.classList.add("selected-price");
+                    priceList.textContent = price;
+                    priceList.title = price; // Add a title attribute for identification
+                    filtersContainer.appendChild(priceList);
+
+                    const icon = document.createElement("img");
+                    icon.src = "D:/site_shop/Desing/imag/icon/Group 120.svg";
+                    icon.alt = "Delete";
+                    icon.style.width = "24px";
+                    icon.style.height = "24px"; 
+                    icon.style.marginLeft = "5px"
+                    priceList.appendChild(icon);
+
+
+                    icon.addEventListener("click", () => {
+                        filtersContainer.removeChild(priceList);
+                    });
+                }
+            }
+        });
+
+        selectedColors.forEach(color => {
+            if (color !== "all") {
+                // Check if the color filter already exists
+                if (!filtersContainer.querySelector(".selected-color[title='" + color + "']")) {
+                    const colorElement = document.createElement("div");
+                    colorElement.classList.add("selected-color");
+                    colorElement.textContent = color;
+                    colorElement.title = color; // Add a title attribute for identification
+                    filtersContainer.appendChild(colorElement);
+
+                    const icon = document.createElement("img");
+                    icon.src = "D:/site_shop/Desing/imag/icon/Group 120.svg";
+                    icon.alt = "Delete";
+                    icon.style.width = "24px";
+                    icon.style.height = "24px"; 
+                    icon.style.marginLeft = "5px"
+                    colorElement.appendChild(icon);
+
+
+                    icon.addEventListener("click", () => {
+                        filtersContainer.removeChild(colorElement);
+                    });
+                }
+            }
+        });
     });
-    cleatFilter.addEventListener("click", () => {
-        
-        selectedColorsContainer.innerHTML = "";
+
+
+    clearFilter.addEventListener("click", () => {
+        filtersContainer.innerHTML = "";
+        filterNameInput.value = "";
+
+        // Display all products when clearing filters
+        productItems.forEach(product => {
+            product.style.display = "block";
+        });
     });
+
+
+    
 });
